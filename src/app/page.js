06d44cf243e1f981b0a4da5e4f16a19b5fa31a87
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2026-08-31 01:54:24 +0530

    our services fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import HeroSection from "../components/Home/HeroSection";
 import OurWorkSection from "../components/Home/OurWorkSection";
-import OurServicesSection from "../components/Home/OurServicesSection";
 import ProcessSection from "../components/Home/ProcessSection";
 import TestimonialsSection from "../components/Home/TestimonialsSection";
 import CtaSection from "../components/Home/CtaSection";
@@ -11,13 +10,10 @@ export default function Home() {
       {/* 1. Hero Section */}
       <HeroSection />
 
-      {/* 2. Our Work Section (frprotech style carousel) */}
+      {/* 2. Our Work Section */}
       <OurWorkSection />
 
-      {/* 3. Our Services Section (frprotech style sticky sidebar + feature cards) */}
-      <OurServicesSection />
-
-      {/* 4. Process, Testimonials & CTA */}
+      {/* 3. Process, Testimonials & CTA */}
       <ProcessSection />
       <TestimonialsSection />
       <CtaSection />
