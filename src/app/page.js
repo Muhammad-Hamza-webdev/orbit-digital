b0598a249237564d010e6e728c9e7468b34a41f6
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2026-08-31 02:31:06 +0530

    Card Stack fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import HeroSection from "../components/Home/HeroSection";
 import OurWorkSection from "../components/Home/OurWorkSection";
+import CardsStack from "../components/Home/CardsStack";
 import ProcessSection from "../components/Home/ProcessSection";
 import TestimonialsSection from "../components/Home/TestimonialsSection";
 import CtaSection from "../components/Home/CtaSection";
@@ -13,10 +14,14 @@ export default function Home() {
       {/* 2. Our Work Section */}
       <OurWorkSection />
 
-      {/* 3. Process, Testimonials & CTA */}
+      {/* 3. Cards Stack Parallax Section */}
+      <CardsStack />
+
+      {/* 4. Process, Testimonials & CTA */}
       <ProcessSection />
       <TestimonialsSection />
       <CtaSection />
     </>
   );
 }
+
